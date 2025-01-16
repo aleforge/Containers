899d2065d3b513d7fa781514e0165e5cf3d8e02d
wraith1200 <ebrown@aleforge.net>
2025-01-16 12:08:36 -0500

    updating aloft image

--- games/aloft/entrypoint.sh
+++ games/aloft/entrypoint.sh
@@ -7,6 +7,7 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
+
 # Wait for the container to fully initialize
 sleep 1
 
@@ -14,37 +15,19 @@ sleep 1
 TZ=${TZ:-UTC}
 export TZ
 
+# Information output
+echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+echo -e "${YELLOW}Wine Image from AleForge{NC}"
+echo -e "${RED}THIS IMAGE IS LICENSED UNDER AGPLv3${NC}"
+echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+echo -e "${YELLOW}echo -e "${YELLOW}Linux Distribution: ${RED} $(. /etc/os-release ; echo $PRETTY_NAME)${NC}" $(cat /etc/debian_version)${NC}"
+echo -e "${YELLOW}Current timezone: $(cat /etc/timezone)${NC}"
+echo -e "${YELLOW}Wine Version:${NC} ${RED} $(wine --version)${NC}"
+echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+
 # Set environment variable that holds the Internal Docker IP
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
-
-echo -e "${BLUE}----------------------------------------------------------------------------------${NC}"
-    echo -e "${RED}SteamCMD Proton-GE Image by gOOvER - https://discord.goover.dev${NC}"
-	echo -e "${RED}THIS IMAGE IS LICENSED UNDER AGPLv3${NC}"
-    echo -e "${BLUE}----------------------------------------------------------------------------------${NC}"
-    echo -e "${YELLOW}Linux Distribution: ${RED} $(. /etc/os-release ; echo $PRETTY_NAME)${NC}"
-    echo -e "${YELLOW}Kernel: ${RED} $(uname -r)${NC}"
-    echo -e "${YELLOW}Current timezone: ${RED} $(cat /etc/timezone)${NC}"
-    echo -e "${YELLOW}Proton Version: ${RED} $(cat /usr/local/bin/version)${NC}"
-    echo -e "${BLUE}----------------------------------------------------------------------------------${NC}"
-
-# Set environment for Steam Proton
-if [ ! -z ${STEAM_APPID} ]; then
-    mkdir -p /home/container/.steam/steam/steamapps/compatdata/${STEAM_APPID}
-    export STEAM_COMPAT_CLIENT_INSTALL_PATH="/home/container/.steam/steam"
-    export STEAM_COMPAT_DATA_PATH="/home/container/.steam/steam/steamapps/compatdata/${STEAM_APPID}"
-    export WINETRICKS="/usr/sbin/winetricks"
-    export STEAM_DIR="/home/container/.steam/steam/"
-
-else
-    echo -e "${BLUE}----------------------------------------------------------------------------------${NC}"
-    echo -e "${RED}WARNING!!! Proton needs variable STEAM_APPID, else it will not work. Please add it${NC}"
-    echo -e "${RED}Server stops now${NC}"
-    echo -e "${BLUE}----------------------------------------------------------------------------------${NC}"
-    exit 0
-fi
-
-sleep 2
 
 # Switch to the container's working directory
 cd /home/container || exit 1
@@ -69,7 +52,7 @@ if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
 	if [ -f /home/container/DepotDownloader ]; then
 		./DepotDownloader -dir /home/container -username ${STEAM_USER} -password ${STEAM_PASS} -remember-password $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '-os windows' ) -app ${STEAM_APPID} $( [[ -z ${STEAM_BETAID} ]] || printf %s "-beta ${STEAM_BETAID}" ) $( [[ -z ${STEAM_BETAPASS} ]] || printf %s "-betapassword ${STEAM_BETAPASS}" )
 		mkdir -p /home/container/.steam/sdk64
-		./DepotDownloader -dir /home/container/.steam/sdk64 $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '-os windows' ) -app 1007
+		./DepotDownloader -dir /home/container/.steam/sdk64 -os windows -app 1007
 		chmod +x $HOME/*
 
 	else
@@ -81,21 +64,60 @@ else
     echo -e "${BLUE}---------------------------------------------------------------${NC}"
 fi
 
-echo -e "${BLUE}----------------------------------------------------------------------------------${NC}"
-echo -e "${GREEN}Starting Server.... Please wait...${NC}"
-echo -e "${BLUE}----------------------------------------------------------------------------------${NC}"
+if [[ $XVFB == 1 ]]; then
+        Xvfb :0 -screen 0 ${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x${DISPLAY_DEPTH} &
+fi
+
+# Install necessary to run packages
+echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+echo -e "${RED}First launch will throw some errors. Ignore them${NC}"
+echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+mkdir -p $WINEPREFIX
+
+# Check if wine-gecko required and install it if so
+if [[ $WINETRICKS_RUN =~ gecko ]]; then
+        echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+        echo -e "${YELLOW}Installing Wine Gecko${NC}"
+        echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+        WINETRICKS_RUN=${WINETRICKS_RUN/gecko}
+
+        if [ ! -f "$WINEPREFIX/gecko_x86.msi" ]; then
+                wget -q -O $WINEPREFIX/gecko_x86.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86.msi
+        fi
+
+        if [ ! -f "$WINEPREFIX/gecko_x86_64.msi" ]; then
+                wget -q -O $WINEPREFIX/gecko_x86_64.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86_64.msi
+        fi
+
+        wine msiexec /i $WINEPREFIX/gecko_x86.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_install.log
+        wine msiexec /i $WINEPREFIX/gecko_x86_64.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_64_install.log
+fi
+
+# Check if wine-mono required and install it if so
+if [[ $WINETRICKS_RUN =~ mono ]]; then
+        echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+        echo -e "${YELLOW}Installing Wine Mono${NC}"
+        echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+        WINETRICKS_RUN=${WINETRICKS_RUN/mono}
+
+        if [ ! -f "$WINEPREFIX/mono.msi" ]; then
+                wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/9.4.0/wine-mono-9.4.0-x86.msi
+        fi
+
+        wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart /log $WINEPREFIX/mono_install.log
+fi
 
 # List and install other packages
-#for trick in $PROTONTRICKS_RUN; do
-#        echo -e "${BLUE}---------------------------------------------------------------------${NC}"
-#        echo -e "${YELLOW}Installing: ${NC} ${GREEN} $trick ${NC}"
-#        echo -e "${BLUE}---------------------------------------------------------------------${NC}"
-#        protontricks ${STEAM_APPID} $trick
-#done
+for trick in $WINETRICKS_RUN; do
+        echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+        echo -e "${YELLOW}Installing: ${NC} ${GREEN} $trick ${NC}"
+        echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+        winetricks -q $trick
+done
 
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
