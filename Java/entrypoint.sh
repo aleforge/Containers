d13f949f30a42b47c50c13e9a06d4b638239c1b6
wraith1200 <ebrown@aleforge.net>
2025-08-20 18:43:22 -0400

    fixing duplicate entrypoint issue

--- Java/entrypoint.sh
+++ Java/entrypoint.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
-# variable format of "${VARIABLE}" before evaluating the string and automatically
-# replacing the values.
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
-
-# Display the command we're running in the output, and then execute it with the env
-# from the container itself.
-printf "\033[1m\033[33mcontainer@pelican~ \033[0m%s\n" "$PARSED"
-# shellcheck disable=SC2086
-eval ${PARSED}
-
-#!/bin/bash
-
 clear
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -33,7 +20,7 @@ echo -e "${BLUE}----------------------------------------------------------------
 echo -e "${YELLOW}Java Image from AleForge${NC}"
 echo -e "${RED}THIS IMAGE IS LICENSED UNDER AGPLv3${NC}"
 echo -e "${BLUE}---------------------------------------------------------------------${NC}"
-echo -e "${YELLOW}echo -e "${YELLOW}Linux Distribution: ${RED} $(. /etc/os-release ; echo $PRETTY_NAME)${NC}" $(cat /etc/debian_version)${NC}"
+echo -e "${YELLOW}"${YELLOW}Linux Distribution: ${RED} $(. /etc/os-release ; echo $PRETTY_NAME)${NC}" $(cat /etc/debian_version)${NC}"
 echo -e "${YELLOW}Current timezone: $(cat /etc/timezone)${NC}"
 echo -e "${YELLOW}Java Version:${NC} ${RED} $(java -version)${NC}"
 echo -e "${BLUE}---------------------------------------------------------------------${NC}"
@@ -45,17 +32,13 @@ export INTERNAL_IP
 # Switch to the container's working directory
 cd /home/container || exit 1
 
-# Print Java version
-printf "\033[1m\033[33mcontainer@aleforge~ \033[0mjava -version\n"
-java -version
-
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
 # variable format of "${VARIABLE}" before evaluating the string and automatically
 # replacing the values.
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+PARSED=$(echo "$STARTUP" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 
-# Display the command we're running in the output, and then execute it with the env
-# from the container itself.
-printf "\033[1m\033[33mcontainer@aleforge~ \033[0m%s\n" "$PARSED"
+# Display the command we're running in the output, and then execute it with eval
+printf "\033[1m\033[33maleforge~ \033[0m"
+echo "$PARSED"
 # shellcheck disable=SC2086
-exec env ${PARSED}
+eval "$PARSED"
