77a8bda2d78e7132c554b4cb66f0ab80fd9d8722
wraith1200 <ebrown@aleforge.net>
2026-01-13 10:13:41 -0500

    adding info output

--- games/hytale/start.sh
+++ games/hytale/start.sh
@@ -12,6 +12,16 @@ YELLOW='\033[1;33m'
 RED='\033[1;31m'
 NC='\033[0m'
 
+# Information output
+echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+echo -e "${YELLOW}Hytale Image from AleForge${NC}"
+echo -e "${RED}THIS IMAGE IS LICENSED UNDER AGPLv3${NC}"
+echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+echo -e "${YELLOW}"${YELLOW}Linux Distribution: ${RED} $(. /etc/os-release ; echo $PRETTY_NAME)${NC}" $(cat /etc/debian_version)${NC}"
+echo -e "${YELLOW}Current timezone: $(cat /etc/timezone)${NC}"
+echo -e "${YELLOW}Java Version:${NC} ${RED} $(java -version)${NC}"
+echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+
 # ─────────────────────────────────────────────
 # Egg variables / defaults
 # ─────────────────────────────────────────────
