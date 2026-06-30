7565cafd0f1fd08d0135431e1db97ea5a04e4c15
wraith1200 <ebrown@aleforge.net>
2026-06-29 22:16:38 -0400

    adding a new proton dev for testing proton from server volume

---
+++ steamcmd/proton-dev/entrypoint.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2021 Matthew Penner
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+clear
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+# Wait for the container to fully initialize
+sleep 1
+
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Information output
+echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+echo -e "${RED}SteamCMD Proton Dev Image${NC}"
+echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+echo -e "${YELLOW}Linux Distribution: ${RED} $(. /etc/os-release ; echo $PRETTY_NAME) $(cat /etc/debian_version)${NC}"
+echo -e "${YELLOW}Current timezone: ${RED} $(cat /etc/timezone)${NC}"
+echo -e "${GREEN}Maintained by AleForge.net${NC}"
+echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+
+# Copy Proton-GE from the read-only image layer into the writable server volume.
+# Pterodactyl/AleForge use ReadonlyRootfs; Proton needs to write dist.lock and
+# default_pfx under its install directory at first run.
+setup_writable_proton() {
+    local proton_source="/opt/ProtonGE"
+    local proton_compat_dir="/home/container/.steam/steam/compatibilitytools.d"
+    local proton_install_dir="${proton_compat_dir}/ProtonGE"
+
+    if [ ! -d "$proton_source" ]; then
+        echo -e "${RED}Proton-GE source not found at ${proton_source}${NC}" >&2
+        return 1
+    fi
+
+    mkdir -p "$proton_compat_dir"
+
+    local needs_install=0
+    if [ ! -x "${proton_install_dir}/proton" ]; then
+        needs_install=1
+    elif [ -f "${proton_source}/version" ] && ! cmp -s "${proton_source}/version" "${proton_install_dir}/version" 2>/dev/null; then
+        needs_install=1
+    fi
+
+    if [ "$needs_install" -eq 1 ]; then
+        echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+        echo -e "${YELLOW}Installing Proton-GE to writable path:${NC}"
+        echo -e "${YELLOW}  ${proton_install_dir}${NC}"
+        echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+        rm -rf "$proton_install_dir"
+        cp -a "$proton_source" "$proton_install_dir"
+    fi
+}
+
+if [ -f "/usr/local/bin/proton" ] && [ -d "/opt/ProtonGE" ]; then
+    setup_writable_proton || exit 1
+fi
+
+# Set environment for Steam Proton
+if [ -f "/usr/local/bin/proton" ]; then
+    if [ ! -z ${SRCDS_APPID} ]; then
+	    mkdir -p /home/container/.steam/steam/steamapps/compatdata/${SRCDS_APPID}
+        export STEAM_COMPAT_CLIENT_INSTALL_PATH="/home/container/.steam/steam"
+        export STEAM_COMPAT_DATA_PATH="/home/container/.steam/steam/steamapps/compatdata/${SRCDS_APPID}"
+        export WINETRICKS="/usr/sbin/winetricks"
+
+    else
+        echo -e "${BLUE}----------------------------------------------------------------------------------${NC}"
+        echo -e "${RED}WARNING!!! Proton needs variable SRCDS_APPID, else it will not work. Please add it${NC}"
+        echo -e "${RED}Server stops now${NC}"
+        echo -e "${BLUE}----------------------------------------------------------------------------------${NC}"
+        exit 0
+        fi
+fi
+
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+# -----------------------------------------------------------------------------
+# NSS Wrapper Setup (Safe check for AleForge images)
+# -----------------------------------------------------------------------------
+# Fakes the /etc/passwd entry for the dynamic Pterodactyl container user.
+# Prevents C++ games (like Avorion) from crashing on getpwuid() checks.
+if [ -f "/usr/lib/x86_64-linux-gnu/libnss_wrapper.so" ]; then
+    export USER_ID=$(id -u)
+    export GROUP_ID=$(id -g)
+    export NSS_WRAPPER_PASSWD=/tmp/passwd
+    export NSS_WRAPPER_GROUP=/etc/group
+
+    # Create the mock passwd file using the current active user ID
+    echo "container:x:${USER_ID}:${GROUP_ID}:container:/home/container:/bin/bash" > ${NSS_WRAPPER_PASSWD}
+
+    # Load the NSS wrapper library
+    export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libnss_wrapper.so
+fi
+# -----------------------------------------------------------------------------
+
+## just in case someone removed the defaults.
+if [ "${STEAM_USER}" == "" ]; then
+    echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+    echo -e "${YELLOW}Steam user is not set. ${NC}"
+    echo -e "${YELLOW}Using anonymous user.${NC}"
+    echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+    STEAM_USER=anonymous
+    STEAM_PASS=""
+    STEAM_AUTH=""
+else
+    echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+    echo -e "${YELLOW}user set to ${STEAM_USER} ${NC}"
+    echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+fi
+
+## if auto_update is not set or to 1 update
+if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then 
+    # Update Source Server
+    if [ ! -z ${SRCDS_APPID} ]; then
+	    if [ "${STEAM_USER}" == "anonymous" ]; then
+            ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update ${SRCDS_APPID} $( [[ -z ${STEAM_BETAID} ]] || printf %s "-beta ${STEAM_BETAID}" ) $( [[ -z ${STEAM_BETAPASS} ]] || printf %s "-betapassword ${STEAM_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
+	    else
+            ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update ${SRCDS_APPID} $( [[ -z ${STEAM_BETAID} ]] || printf %s "-beta ${STEAM_BETAID}" ) $( [[ -z ${STEAM_BETAPASS} ]] || printf %s "-betapassword ${STEAM_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
+	    fi
+        echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+        echo -e "${YELLOW}Starting Server${NC}"
+        echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+    else
+        echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+        echo -e "${YELLOW}No appid set. Starting Server${NC}"
+        echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+    fi
+
+else
+    echo -e "${BLUE}---------------------------------------------------------------${NC}"
+    echo -e "${YELLOW}Not updating game server as auto update was set to 0. Starting Server${NC}"
+    echo -e "${BLUE}---------------------------------------------------------------${NC}"
+fi
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
